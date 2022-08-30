194c79d49ffb2251f5c103914d1f8eed94f94c4e
204g1a0522 <111738674+204g1a0522@users.noreply.github.com>
2022-08-30 11:05:48 +0530

    B.Bhavana sample.c

---
+++ OSLAB/sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Bhavana
+   Roll No: 204G1A0522
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
