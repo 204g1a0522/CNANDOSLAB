7aaf342127a8d5eac3e2dca07948fafdf792764c
204g1a0522 <111738674+204g1a0522@users.noreply.github.com>
2022-11-03 14:23:55 +0530

    B Bhavana created rb.c

---
+++ Exp1`/rb.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Bhavana
+   Roll No: 204G1A0522
+   Title of the Experiment: CPU Scheduling Using Round Robin Algorithm
+   Date of Creation: 13-09-2022
+   Date of Execution: 13-09-2022
+*/
