ec1c73f807f62084f4baeb2541f164418abe2f66
204g1a0522 <111738674+204g1a0522@users.noreply.github.com>
2022-11-03 14:29:25 +0530

    B Bhavana created priority.c

---
+++ Exp1`/priority.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Bhavana
+   Roll No: 204G1A0522
+   Title of the Experiment: CPU Scheduling Using priority Algorithm
+   Date of Creation: 13-09-2022
+   Date of Execution: 13-09-2022
+*/
