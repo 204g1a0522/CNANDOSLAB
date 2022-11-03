e004a777f3061ee194ecbfda584d0ad71d96345f
204g1a0522 <111738674+204g1a0522@users.noreply.github.com>
2022-11-03 14:19:43 +0530

    B Bhavana created sjf.c

---
+++ Exp1`/sjf.c
@@ -0,0 +1,7 @@
+
+/* Name Of the Candidate: B.Bhavana
+   Roll No: 204G1A0522
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 13-09-2022
+   Date of Execution: 13-09-2022
+*/
